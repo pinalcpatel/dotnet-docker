5cb52e9c95de6b6399b7ec81ab2f30d154d6b514
Matt Thalman <mthalman@microsoft.com>
2019-02-25 16:15:42 -0600

    script updates

--- scripts/Get-ImageNames.ps1
+++ scripts/Get-ImageNames.ps1
@@ -1,5 +1,5 @@
 return @{
-    ImageBuilderLinux = "microsoft/dotnet-buildtools-prereqs:image-builder-debian-20190216044810"
-    ImageBuilderWindows = "microsoft/dotnet-buildtools-prereqs:image-builder-nanoserver-20190215204829"
-    TestRunner = "microsoft/dotnet-buildtools-prereqs:debian-stretch-docker-testrunner-63f2145-20184325094343"
+    ImageBuilderLinux = "mcr.microsoft.com/dotnet-buildtools/image-builder:debian-20190216044810"
+    ImageBuilderWindows = "mcr.microsoft.com/dotnet-buildtools/image-builder:nanoserver-20190215204829"
+    TestRunner = "mcr.microsoft.com/dotnet-buildtools/prereqs:debian-stretch-docker-testrunner-63f2145-20184325094343"
 }
